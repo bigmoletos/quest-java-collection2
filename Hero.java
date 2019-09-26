b60ffd6b214dba7d9862f7b563d425e7071780a7
bigmoletos <bigmoletos@yopmail.com>
2019-09-26 13:34:43 +0200

    quete Java collection2 by fd use comparator and compareTo

--- Hero.java
+++ Hero.java
@@ -1,30 +1,46 @@
-public class Hero {
 
-    // attributs
-    private String name;
-    private int age;
+/**
+ * @author franck Desmedt github/bigmoletos
+ * @Description Quete Java Collection02 . Modifie la classe Hero afin
+ *              d'implémenter Comparable. L'ordre de tri par défaut sera sur
+ *              l'attribut name, par ordre croissant. Dans la classe Thanos,
+ *              fait en sorte que la liste de héros soit bien triée comme voulu.
+ *              Dans la classe Thanos, ajoute un Comparator permettant de trier
+ *              la liste de héros sur l'attribut age décroissant.
+ */
+public class Hero implements Comparable<Hero> {
 
-    // constructeurs
-    public Hero(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+	// attributs
+	private String name;
+	private int age;
 
-    // accesseurs (getters)
-    public String getName() {
-        return name;
-    }
+	// constructeurs
+	public Hero(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	// accesseurs (getters)
+	public String getName() {
+		return name;
+	}
 
-    // mutateurs (setters)
-    public void setName(String name) {
-        this.name = name;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	// mutateurs (setters)
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int compareTo(Hero hero) {
+//comparaison entre getName session et getName instancié
+		return this.getName().compareTo(hero.getName());
+	}
 }
--- Thanos.java
+++ Thanos.java
@@ -1,32 +1,45 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        List<Hero> heroes = new ArrayList<>();
-        heroes.add(new Hero("Hulk", 49));
-        heroes.add(new Hero("Black Widow", 34));
-        heroes.add(new Hero("Captain America", 100));
-        heroes.add(new Hero("Thor", 1501));
+		List<Hero> heroes = new ArrayList<>();
+		heroes.add(new Hero("Hulk", 49));
+		heroes.add(new Hero("Black Widow", 34));
+		heroes.add(new Hero("Captain America", 100));
+		heroes.add(new Hero("Thor", 1501));
 
-        // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
+		// TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
 
+		System.out.println("\nOrder by name:");
+		Collections.sort(heroes);
+		showList(heroes);
 
-        System.out.println("\nOrder by name:");
-        showList(heroes);
+		// TODO 2: Add a Comparator and sort by age (descending)
 
-        // TODO 2: Add a Comparator and sort by age (descending)
+		System.out.println("\nOrder by age:");
+//
+		Comparator<Hero> comparator = new Comparator<Hero>() {
 
+			@Override
+			public int compare(Hero hero1, Hero hero2) {
+				// compare age hero2 à age hero1
+				return hero1.getAge() - hero2.getAge();
+			}
+		};
+		Collections.sort(heroes, comparator);
+		// Collections.sort(heroes);
+		showList(heroes);
+	}
 
-        System.out.println("\nOrder by age:");
-        showList(heroes);
-    }
+	private static void showList(List<Hero> heroes) {
+		for (Hero heroe : heroes) {
+			System.out.println(heroe.getName() + ", " + heroe.getAge());
+		}
+	}
 
-    private static void showList(List<Hero> heroes) {
-        for (Hero heroe : heroes) {
-            System.out.println(heroe.getName() + ", " + heroe.getAge());
-        }
-    }
 }
